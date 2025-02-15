274a54d3990769911e53db22710bcaba06589652
Rishav Raj Bhagat <rishavrajbhagat123@gmail.com>
2025-02-15 17:28:44 +0530

    removed redundant codes, fixed type error issues, removed event listener and logic to run soc.connect() infinite in MarketdataSocketExample.py file

--- MarketdataSocketExample.py
+++ MarketdataSocketExample.py
@@ -42,66 +42,60 @@ def on_connect():
 
 # Callback on receiving message
 def on_message(data):
-    print('I received a message!')
+    print('I received a message!' + str(data))
 
 # Callback for message code 1501 FULL
 def on_message1501_json_full(data):
-    print('I received a 1501 Touchline message!' + data)
+    print('I received a 1501 Touchline message!' + str(data))
 
 # Callback for message code 1502 FULL
 def on_message1502_json_full(data):
-    print('I received a 1502 Market depth message!' + data)
+    print('I received a 1502 Market depth message!' + str(data))
 
 # Callback for message code 1505 FULL
 def on_message1505_json_full(data):
-    print('I received a 1505 Candle data message!' + data)
+    print('I received a 1505 Candle data message!' + str(data))
 
 # Callback for message code 1507 FULL
 def on_message1507_json_full(data):
-    print('I received a 1507 MarketStatus data message!' + data)
+    print('I received a 1507 MarketStatus data message!' + str(data))
 
 # Callback for message code 1510 FULL
 def on_message1510_json_full(data):
-    print('I received a 1510 Open interest message!' + data)
+    print('I received a 1510 Open interest message!' + str(data))
 
 # Callback for message code 1512 FULL
 def on_message1512_json_full(data):
-    print('I received a 1512 Level1,LTP message!' + data)
+    print('I received a 1512 Level1,LTP message!' + str(data))
 
 # Callback for message code 1105 FULL
 def on_message1105_json_full(data):
-    print('I received a 1105, Instrument Property Change Event message!' + data)
+    print('I received a 1105, Instrument Property Change Event message!' + str(data))
   # Callback for message code 1105 FULL
 def on_message1105_json_partial(data):
-    print('I received a 1105, Instrument Property Change Event message!' + data)
+    print('I received a 1105, Instrument Property Change Event message!' + str(data))
 
 
 
 # Callback for message code 1501 PARTIAL
 def on_message1501_json_partial(data):
-    print('I received a 1501, Touchline Event message!' + data)
+    print('I received a 1501, Touchline Event message!' + str(data))
 
 # Callback for message code 1502 PARTIAL
 def on_message1502_json_partial(data):
-    print('I received a 1502 Market depth message!' + data)
+    print('I received a 1502 Market depth message!' + str(data))
 
 # Callback for message code 1505 PARTIAL
 def on_message1505_json_partial(data):
-    print('I received a 1505 Candle data message!' + data)
+    print('I received a 1505 Candle data message!' + str(data))
 
 # Callback for message code 1510 PARTIAL
 def on_message1510_json_partial(data):
-    print('I received a 1510 Open interest message!' + data)
+    print('I received a 1510 Open interest message!' + str(data))
 
 # Callback for message code 1512 PARTIAL
 def on_message1512_json_partial(data):
-    print('I received a 1512, LTP Event message!' + data)
-
-
-
-# Callback for message code 1105 PARTIAL
-def on_message1105_json_partial(data):
-    print('I received a 1105, Instrument Property Change Event message!' + data)
+    print('I received a 1512, LTP Event message!' + str(data))
 
 # Callback for disconnection
 def on_disconnect():
@@ -134,17 +128,8 @@ soc.on_disconnect = on_disconnect
 soc.on_error = on_error
 
 
-# Event listener
-el = soc.get_emitter()
-el.on('connect', on_connect)
-el.on('1501-json-full', on_message1501_json_full)
-el.on('1502-json-full', on_message1502_json_full)
-el.on('1507-json-full', on_message1507_json_full)
-el.on('1512-json-full', on_message1512_json_full)
-el.on('1105-json-full', on_message1105_json_full)
-el.on('1105-json-partial', on_message1105_json_full)
-
-
 # Infinite loop on the main thread. Nothing after this will run.
 # You have to use the pre-defined callbacks to manage subscriptions.
 soc.connect()
+while True:
+    pass
